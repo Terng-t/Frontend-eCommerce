f791a74a49f4ec6eca7250d644dc6e5cea058abf
Rakthai <dengthana@gmail.com>
2024-08-26 21:59:34 +0700

    Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./pages/home/home";
+import Home from "./pages/home/Home";
 import Cart from "./pages/cart/Cart";
 
 function App() {
